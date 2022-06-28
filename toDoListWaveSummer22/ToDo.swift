16693350c65165539f0359a0c7ab57549ef3c1f4
radyah alam <radyahalam@gmail.com>
2022-06-28 11:37:58 -0400

    Initial Commit

---
+++ toDoListWaveSummer22/ToDo.swift
@@ -0,0 +1,10 @@
+//
+//  ToDo.swift
+//  toDoListWaveSummer22
+//
+//  Created by Radyah Alam on 6/27/22.
+//
+
+import UIKit
+
+class ToDo ( var name )
---
+++ toDoListWaveSummer22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDoListWaveSummer22
+//
+//  Created by Radyah Alam on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
